5bf61051e97f6c27e4c88fd7bc2522900e8abf8e
RahulSiddavaram <srjk7572@gmail.com>
2024-01-25 13:34:45 +0530

    roboshop script added

--- mongo.sh
+++ mongo.sh
@@ -33,22 +33,22 @@ cp mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOGFILE
 
 VALIDATE $? "Copied MONGODB Repo into yum.repo.d"
 
-yum install mongodb-org -y &>>$LOGFILE
+yum install mongodb-org -y &>> $LOGFILE
 
 VALIDATE $? "Installation of mongo-org"
 
-systemctl enable mongod &>>$LOGFILE
+systemctl enable mongod &>> $LOGFILE
 
 VALIDATE $? "Enabling MongoDB"
 
-systemctl start mongod &>>$LOGFILE
+systemctl start mongod &>> $LOGFILE
 
 VALIDATE $? "Starting MongoDB"
 
-sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>$LOGFILE
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>> $LOGFILE
 
 VALIDATE $? "Edited MongoDB Conf"
 
-systemctl restart mongod &>>$LOGFILE
+systemctl restart mongod &>> $LOGFILE
 
 VALIDATE $? "Restarting MongoDB"
