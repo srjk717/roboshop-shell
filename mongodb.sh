6e36afce3c1a1a30553370d942ec805e6ae8a1f1
RahulSiddavaram <srjk7572@gmail.com>
2024-01-25 13:58:57 +0530

    roboshop script added

--- mongodb.sh
+++ mongodb.sh
@@ -4,12 +4,11 @@ DATE=$(date +%F)
 LOGSDIR=/tmp
 SCRIPT_NAME=$0
 LOGFILE=$LOGSDIR/$0-$DATE.log
+USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 Y="\e[33m"
-
-USERID=$(id -u)
 
 if [ $USERID -ne 0 ];
 then 
@@ -22,17 +21,16 @@ VALIDATE(){
     then
         echo -e "$2 ... $R FAILURE $N"
         exit 1
-    else    
+    else
         echo -e "$2 ... $G SUCCESS $N"
     fi
 }
-
 
 cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
 
 VALIDATE $? "Copied MONGODB Repo into yum.repos.d"
 
-yum install mongod-org -y &>> $LOGFILE
+yum install mongodb-org -y &>> $LOGFILE
 
 VALIDATE $? "Installation of mongo-org"
 
